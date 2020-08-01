6d7ca49ecb8cae8597bf49c11deb4f11d8dedb39
sristham <46941562+sristham@users.noreply.github.com>
2020-07-31 23:04:30 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import { bookInfo } from '../../app/bookInfo';
+
+export const MYBOOKS: bookInfo[] = [
+    { bookName: 'All The Light We Cannot See', authorName: 'Anthony Doerr', bookGenre: 'Historical', yearPublished: 2014 , picture: 'assets/Images/allthelightwecannotsee.jpg' },
+    { bookName: 'Astrophysics for People in a Hurry', authorName: 'Neil Degrasse Tyson', bookGenre: 'Non-Fiction', yearPublished: 2017 , picture: 'assets/Images/astrophysicsforpeopleinahurry.jpg' },
+    { bookName: 'Lafcadio', authorName: 'Shel Silverstein', bookGenre: 'Fiction', yearPublished: 1963 , picture: 'assets/Images/lafcadio.jpg' },
+    { bookName: 'Twelve Angry Men', authorName: 'Reginald Rose', bookGenre: 'Drama', yearPublished: 1957 , picture: 'assets/Images/twelveangrymen.jpg' },
+];
